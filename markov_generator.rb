98d8e3097c77b09300d319a3b8aa923bdd0a0f17
r3drighthand <ryanmccool13@gmail.com>
2018-01-10 18:21:33 -0600

    First commit

--- markov_generator.rb
+++ markov_generator.rb
@@ -1,3 +1,9 @@
+# Break the input text into words. Ruby's String class has a #split method that works great for this.
+# Build a hash that keeps track of all of the words that appear immediately after each word in the the input.
+# Choose a random word from the text, and then choose a random word from the list of words that followed that word.
+# Choose a random word that follows the following word from the last step, and repeat.
+
+
 class MarkovGenerator
   def self.from_file file
     from_text File.read(file)
