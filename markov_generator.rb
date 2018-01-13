4641f666e3785310363c5c39ad0e28ecd907c657
r3drighthand <ryanmccool13@gmail.com>
2018-01-12 20:49:09 -0600

    Add code to treat punctuation as words

--- markov_generator.rb
+++ markov_generator.rb
@@ -1,8 +1,4 @@
-# Break the input text into words. Ruby's String class has a #split method that works great for this.
-# Build a hash that keeps track of all of the words that appear immediately after each word in the the input.
-# Choose a random word from the text, and then choose a random word from the list of words that followed that word.
-# Choose a random word that follows the following word from the last step, and repeat.
-
+require 'pp'
 
 class MarkovGenerator
   def self.from_file(file)
@@ -12,32 +8,34 @@ class MarkovGenerator
   def self.from_text text
     freq_hash = Hash.new {|hash, key| hash[key] = [] }
 
+    # turning punctuation into words
+    text.gsub!(".", " .")
+    text.gsub!("!", " !")
+    text.gsub!("?", " ?")
+
     text.split.each_cons(2) do |word, following_word|
       freq_hash[word] << following_word
     end
 
-
-    require 'pp'
-    pp freq_hash
+    # pp freq_hash
     current_word = freq_hash.keys.sample
     sentence = [current_word]
-    # last_word = ''
 
-    12.times do
+    20.times do
       p current_word
       current_word = freq_hash[current_word].sample
 
       sentence << current_word
     end
 
-    # add logic for handling punctuation here?
-    # separate punctuation from words
+    # punctuation = sentence.select do |word|
+    #   word == " !" || word == " ." || word == " ?"
+    # end
+
     sentence.join(" ")
   end
-end
 
-# Treat sentence-ending punctuation (ie, ., ?, and !) as words themselves.
-# if word[-1] == '.' || '?' || '!'
+end
 
 # use scan?
 # end_with?
